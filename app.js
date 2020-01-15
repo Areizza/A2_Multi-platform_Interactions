8060498f5ab9c0b4d74d80d13dec7ad894ae6d0c
Priscilla Lo <lo000049@algonquinlive.com>
2020-01-15 09:41:01 -0500

    First initialization

---
+++ app.js
@@ -0,0 +1,17 @@
+//use const or let
+const express   = require("express"); //include library to access as object
+const app       = express(); //initialize code
+const http      = require("http"); //http is built-in
+const server    = http.createServer(app); //pass express app into created server to initialize
+
+const PORT      = 1111; //default unsecured is 80 if not defined; some ports are reserved
+server.listen(PORT); //port to access server
+
+app.use(express.static(__dirname + "\public")); //describe where files are located
+
+console.log("Listening on port: " + PORT);
+
+//root route
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + "/public/index.html"); //serve index.html
+});
