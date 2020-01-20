c440c9e39be36dad8885b9cb30edaaa562efebc7
Priscilla Lo <priscillalo@cmail.carleton.ca>
2020-01-20 11:01:34 -0500

    Create cheese and create pepperoni depending on counter; can delete pepperonis

--- public/js/create-topping-component.js
+++ public/js/create-topping-component.js
@@ -1,15 +1,32 @@
 'use strict' //strict javascript
 
-AFRAME.registerComponent('create-cheese-component', {
+AFRAME.registerComponent('create-topping-component', {
     schema: {},
     init: function()  {
         const Context_AF = this; //this refers to "this" component, keep this context
+
+        //count how many toppings this pizza currently has
         let counter = 0;
+        window.cheeseCount = 0;
+
         //add event listener for "click" event on whatever entity has this component
         Context_AF.el.addEventListener('click', function(event) {
             counter ++;
             console.log("clicked!!!!!!!! " + counter);
-            Context_AF.createCheese(counter);
+            console.log(window.cheeseCount);
+            console.log(Context_AF.el.id);
+
+            //cheese event
+            if(Context_AF.el.id=="cheese_button") {
+                window.cheeseCount++;
+                Context_AF.createCheese(counter);
+            }
+
+            //pepperoni event
+            if(Context_AF.el.id=="pepperoni_button") {
+                Context_AF.createPepperoni(window.cheeseCount);
+            }
+            
         });
 
         //make button larger on hover
@@ -49,5 +66,30 @@ AFRAME.registerComponent('create-cheese-component', {
         //attach to scene
         let scene = document.querySelector('a-scene');
         scene.appendChild(cheeseElem);
-    }
+    },
+
+    //custom function for adding pepperonis onto the pizza
+    createPepperoni: function(counter) {
+        const Context_AF = this; //be careful of "this"!
+
+        //create an html element that makes the pepperoni
+        let pepperoniElem = document.createElement('a-entity'); //create element by code
+        pepperoniElem.setAttribute('class', 'clickable');
+        pepperoniElem.setAttribute('geometry', 'primitive:cylinder; radius:0.1; height:0.01;');
+        pepperoniElem.setAttribute('material', 'color:#ca2521;'); //set material/texture
+
+        //random position on cheese/pizza area
+        pepperoniElem.setAttribute('position', {x:(Math.random() * 0.9) - 0.45, y: 0.63 + (counter * 0.014), z:(Math.random() * 0.9) - 0.45});
+
+        //random scale
+        const randScale = 0.8 + (Math.random() * 0.3);
+        pepperoniElem.setAttribute('scale', {x:randScale, y:randScale, z:randScale});
+
+        //can be deleted
+        pepperoniElem.setAttribute('delete-topping-component', "");
+
+        //attach to scene
+        let scene = document.querySelector('a-scene');
+        scene.appendChild(pepperoniElem);
+    },
 });
--- public/js/delete-topping-component.js
+++ public/js/delete-topping-component.js
@@ -1,28 +1,28 @@
 'use strict'
 
-AFRAME.registerComponent('delete-cow-component', {
+AFRAME.registerComponent('delete-topping-component', {
     schema: {},
     init: function()  {
         const Context_AF = this; //this refers to "this" component, keep this context
-        const Cow = Context_AF.el.object3D;
+        const Topping = Context_AF.el.object3D;
 
         //add event listener for "click" event on whatever entity has this component
         Context_AF.el.addEventListener('click', function(event) {
             console.log("DELETE ME");
-            Context_AF.deleteCow();
+            Context_AF.deleteTopping();
         });
 
         //make button larger on hover
         Context_AF.el.addEventListener('mouseenter', function(event) {
-            Context_AF.el.object3D.scale.set(Cow.scale.x * 1.1, Cow.scale.y * 1.1, Cow.scale.z * 1.1);
+            Context_AF.el.object3D.scale.set(Topping.scale.x * 1.1, Topping.scale.y * 1.1, Topping.scale.z * 1.1);
         });
 
         Context_AF.el.addEventListener('mouseleave', function(event) {
-            Context_AF.el.object3D.scale.set(Cow.scale.x / 1.1, Cow.scale.y / 1.1, Cow.scale.z / 1.1);
+            Context_AF.el.object3D.scale.set(Topping.scale.x / 1.1, Topping.scale.y / 1.1, Topping.scale.z / 1.1);
         })
     },
 
-    deleteCow: function() {
+    deleteTopping: function() {
         const Context_AF = this; //be careful of "this"!
         Context_AF.el.parentNode.removeChild( Context_AF.el ); //only a parent can delete you
     }
